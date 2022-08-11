e847f8385a18c8c98b1d6ccef122532cb427a1f0
HopNgo <ngoxuanhop254@gmail.com>
2022-08-11 16:22:38 +0700

    initial sample data

--- src/components/BoardContent/BoardContent.tsx
+++ src/components/BoardContent/BoardContent.tsx
@@ -1,17 +1,35 @@
 import CardColumn from "components/CardColumn/CardColumn";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BoardContent.scss";
+import { initalData } from "actions/initalData";
+import IBoard from "interface/IBoard";
+import IColumn from "interface/ICoLumn";
+import { mapOrder } from "utilities/sorts";
 
 const BoardContent = () => {
+  const [board, setBoard] = useState<IBoard>();
+  const [columns, setColumns] = useState<IColumn[]>([]);
+
+  useEffect(() => {
+    const boardFromDB = initalData.boards.find(
+      (board: IBoard) => (board.id = "board-1")
+    );
+    if (boardFromDB) {
+      setBoard(boardFromDB);
+
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
+    }
+  }, []);
+
+  if (board === undefined) {
+    return <div className="">Board Not found</div>;
+  }
+
   return (
     <div className="board-content">
-      <CardColumn />
-      <CardColumn />
-      <CardColumn />
-      <CardColumn />
-      <CardColumn />
-      <CardColumn />
-      <CardColumn />
+      {columns.map((column: IColumn) => (
+        <CardColumn key={column.id} column={column} />
+      ))}
     </div>
   );
 };
