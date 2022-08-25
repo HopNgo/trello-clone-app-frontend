04ceaa7d34df679ba47ec87536ea25663150efbe
HopNgo <ngoxuanhop254@gmail.com>
2022-08-25 15:30:56 +0700

    add navbar app

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 import "./AppBar.scss";
 
 const AppBar = () => {
-  return <nav className="navbar-app">App Bar</nav>;
+  return (
+    <nav className="navbar-app">
+      <div className="navbar-app-actions">
+        <i className="fa fa-bars navbar-app-icon" aria-hidden="true"></i>
+        <Form.Control
+          type="text"
+          size="sm"
+          placeholder="Enter title board..."
+          className="input-board"
+        />
+        <i className="fa fa-search navbar-app-icon"></i>
+      </div>
+      <div className="navbar-app-trello">Trello Clone App By Hop Ngo</div>
+    </nav>
+  );
 };
 
 export default AppBar;
